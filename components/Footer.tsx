d4a4d9cac7e3627d6099960b89048e5b1ef00ac5
Max Antony <maxpacami@gmail.com>
2022-09-06 22:41:13 -0500

    update navbar and footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,7 +3,7 @@ import { FC } from "react";
 export const Footer: FC = () => {
   return (
     <footer>
-      Like
+      Papitas Like
     </footer>
   )
 }
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,20 +1,31 @@
 import Link from "next/link";
 import { FC } from "react";
 
+type NavElement = { title: string, path: string }
+
+const navElements: NavElement[] = [
+  { title: 'Inicio', path: '/' },
+  { title: 'Historia', path: '/history' },
+  { title: 'Contacto', path: '/contact' },
+]
+
+const NavItem: FC<{ navElement: NavElement }> = ({ navElement }) => {
+  return (
+    <li>
+      <Link href={navElement.path}>
+        <a className="p-4">
+          {navElement.title}
+        </a>
+      </Link>
+    </li>
+  )
+}
+
 export const Navbar: FC = () => {
   return (
     <nav>
       <ul className="flex">
-        <li>
-          <Link href={'/'}>
-            Inicio
-          </Link>
-        </li>
-        <li>
-          <Link href={'/contact'}>
-            Contacto
-          </Link>
-        </li>
+        {navElements.map(ne => (<NavItem navElement={ne} />))}
       </ul>
     </nav>
   )
